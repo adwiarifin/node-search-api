9f2ca7e8192c7b19bc82b4a2b56c5092660dc9ce
Adwi Arifin <adwi@itmi.id>
2020-12-24 10:58:47 +0700

    step 11. add refresh token

--- src/services/auth/routes.ts
+++ src/services/auth/routes.ts
@@ -1,27 +1,59 @@
 import { Request, Response } from "express";
-import { authenticate, registerNewUser } from "./AuthController";
+import {
+  authenticate,
+  registerNewUser,
+  generateAccessToken,
+} from "./AuthController";
+
+const options = {
+  path: "/api/v1/refresh",
+  maxAge: 1000 * 60 * 60 * 24 * 90,
+  httpOnly: true,
+  secure: false,
+  signed: false,
+};
 
 export default [
   {
-    path: '/api/v1/signup',
-    method: 'post',
+    path: "/api/v1/signup",
+    method: "post",
     handler: [
       async (req: Request, res: Response) => {
         const { email, password } = req.body;
-        const token = registerNewUser(email, password);
-        res.status(200).send({ auth: true, token });
-      }
-    ]
+        const { accessToken, refreshToken } = registerNewUser(email, password);
+
+        res
+          .cookie("refreshToken", refreshToken, options)
+          .status(200)
+          .send({ auth: true, accessToken });
+      },
+    ],
   },
   {
-    path: '/api/v1/signin',
-    method: 'post',
+    path: "/api/v1/signin",
+    method: "post",
     handler: [
       async (req: Request, res: Response) => {
         const { email, password } = req.body;
-        const token = authenticate(email, password);
-        res.status(200).send({ auth: true, token });
-      }
-    ]
-  }
+        const { accessToken, refreshToken } = authenticate(email, password);
+
+        res
+          .cookie("refreshToken", refreshToken, options)
+          .status(200)
+          .send({ auth: true, accessToken });
+      },
+    ],
+  },
+  {
+    path: "/api/v1/refresh/token",
+    method: "get",
+    handler: [
+      async (req: Request, res: Response) => {
+        const { refreshToken } = req.cookies;
+        const accessToken = generateAccessToken(refreshToken);
+
+        res.status(200).send({ auth: true, accessToken });
+      },
+    ],
+  },
 ];
